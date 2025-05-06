8aaf2b014241963e30c25112fe2cf180981cbed3
Rishiikesh <rishiikeshsk@gmail.com>
2025-05-06 23:34:26 +0530

    /Healthz endpoint is added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+
 func main(){
 	mux:=http.NewServeMux()
 	server:=&http.Server{
@@ -12,14 +13,23 @@ func main(){
 		Addr: ":8080",
 	}
 
-	mux.Handle("/",http.FileServer(http.Dir(".")))
+	mux.Handle("/app/",http.StripPrefix("/app",http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz",func(w http.ResponseWriter,req *http.Request){
+		w.Header().Set("Content-Type","text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+		
+	})
+
+
 
 	log.Println("Server started at port 8080")
 
 	err:=http.ListenAndServe(server.Addr,server.Handler)
 
 	if(err!=nil){
-		log.Println("Server Closed")
+		log.Println("Server Closed",err)
 	}
 
 }
