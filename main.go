d1a6c6c467743ca2056b4a388378f24cead59801
Rishiikesh <rishiikeshsk@gmail.com>
2025-05-07 16:11:40 +0530

    Added json response in main.go

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -36,6 +38,78 @@ func (ac *apiConfig) resetCount (w http.ResponseWriter,req *http.Request){
 	ac.fileServerHits.Swap(0)
 }
 
+
+
+func validate_chirp(w http.ResponseWriter,req *http.Request){
+	type returnError struct{
+		Error string `json:"error"`
+	}
+	type returnVal struct{
+		CleanedBody string `json:"cleaned_body"`
+	}
+	type parameters struct{
+		Body string `json:"body"`
+	}
+	respError:=returnError{
+		Error: "Something went wrong",
+	}
+	anotherRespError:=returnError{
+		Error: "Chirp is too long",
+	}
+	respBody:=returnVal{}
+
+	decoder:=json.NewDecoder(req.Body)
+	params:=parameters{}
+	err:=decoder.Decode(&params)
+
+	if err!=nil{
+		dat,err:=json.Marshal(respError)
+		if err!=nil{
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(500)
+		w.Write([]byte(dat))
+		return 
+	}
+
+	if len(params.Body)>140{
+		dat,err:=json.Marshal(anotherRespError)
+		if err!=nil{
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write([]byte(dat))
+		return 
+	}
+
+	arrBody:=strings.Split(params.Body, " ")
+
+	for i,val :=range arrBody{
+		comp:=strings.ToLower(val)
+		if(comp=="kerfuffle" || comp=="sharbert" || comp=="fornax"){
+			arrBody[i]=strings.Repeat("*",4)
+		}
+	}
+
+	respBody.CleanedBody=strings.Join(arrBody, " ")
+
+	dat,err:=json.Marshal(respBody)
+
+	if err!=nil{
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
+	w.Write([]byte(dat))
+	
+
+}
+
 func main(){
 	mux:=http.NewServeMux()
 	server:=&http.Server{
@@ -49,11 +123,13 @@ func main(){
 		w.Header().Set("Content-Type","text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+
 		
 	})
 
 	mux.HandleFunc("GET /admin/metrics",ac.getCount)
 	mux.HandleFunc("POST /admin/reset",ac.resetCount)
+	mux.HandleFunc("POST /api/validate_chirp",validate_chirp)
 
 
 
