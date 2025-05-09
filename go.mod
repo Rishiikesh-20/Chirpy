ad11d0e9558fa0ea73080b9d860f38c908236565
Rishiikesh <rishiikeshsk@gmail.com>
2025-05-09 15:26:39 +0530

    Added JWT Logic in authentication.go file

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/Rishiikesh-20/Chirpy
 go 1.24.2
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.38.0
 )
--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
 
 
 
@@ -12,4 +19,47 @@ func HashPassword(password string) (string,error){
 func CheckPasswordHash(hash,password string) error{
 	err:=bcrypt.CompareHashAndPassword([]byte(hash),[]byte(password))
 	return err
+}
+
+func MakeJWT(userId int,tokenSecret string,expiresIn time.Duration) (string,error) {
+	claims:=jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: strconv.Itoa((userId)),
+	}
+	token:=jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+
+	signedToken,err:=token.SignedString([]byte(tokenSecret))
+
+	if err!=nil{
+		return "",err
+	}
+	return signedToken,nil
+	
+}
+
+func ValidateJWT(tokenstring,tokenSecret string) (int,error) {
+	claims:=&jwt.RegisteredClaims{}
+	token,err:=jwt.ParseWithClaims(tokenstring,claims,func(token *jwt.Token)(interface{},error){
+		if token.Method!=jwt.SigningMethodHS256{
+			return nil,errors.New("signing method is incorrect")
+		}
+		return []byte(tokenSecret),nil
+
+	})
+	if err!=nil{
+		return 0,err
+	}
+
+	if !token.Valid{
+		return 0,errors.New("invalid token")
+	}
+	userId,err:=strconv.Atoi(claims.Subject)
+
+	if err!=nil{
+		return 0,errors.New("invalid userid in token subject")
+	}
+
+	return userId,nil
 }
